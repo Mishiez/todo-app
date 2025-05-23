398cdff5f0751a79b1392e7b3697a016b4785da3
Michelle Gathoni <michellegathonimain@gmail.com>
2025-05-23 05:17:16 +0300

    Initial commit with a logic behind the TodoApp and some styling using tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Tailwind CSS is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
